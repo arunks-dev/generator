1257a8e2b261932bd1f1546cf400ec200580c9a2
arunks-dev <63200086+arunks-dev@users.noreply.github.com>
2020-05-25 12:16:05 +0100

    added files

---
+++ script.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient")
+
+
+
+function bodyGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+/*color1.addEventListener("input", function() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+})*/
+
+
+color1.addEventListener("input", bodyGradient)
+
+// Don't need to call the function in event listener as 
+//it is already triggered by the input event. 
+
+
+color2.addEventListener("input", bodyGradient)
+
+
+
+//BONUS: Add a random button which generates two random numbers for the colour inputs.
